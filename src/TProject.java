d53679da28b0f80be0458d9f033cc0e5924b0466
Jacob Graves <jgraves3@unm.edu>
2022-09-01 14:30:58 -0700

    Initial Commit

---
+++ src/TProject.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TProject extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
