9c2058cb30361638f82954c24e68e2e9316f07b3
Jacob Graves <jgraves3@unm.edu>
2022-09-02 23:48:12 -0700

    Game actually runs in main now, but I had to take away random colors to do that. So now I need to figure that out.

--- src/mytiles/TProject.java
+++ src/mytiles/TProject.java
@@ -1,59 +1,89 @@
 package mytiles;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-public class TProject extends Application {
+import java.util.concurrent.atomic.AtomicReference;
 
+public class TProject extends Application {
 
     public static final int TILE_SIZE = 100;
     public static final int BOARD_WIDTH = 5;
     public static final int BOARD_HEIGHT = 6;
 
+    private int mouseClick = 0;
+    Tiles tiles1 = new Tiles();
+    Tiles tiles2 = new Tiles();
+
 
     private Parent theGame() {
         GridPane root = new GridPane();
-        root.setPrefSize(BOARD_WIDTH * TILE_SIZE, BOARD_HEIGHT * TILE_SIZE);
+            root.setPrefSize(BOARD_WIDTH * TILE_SIZE, BOARD_HEIGHT * TILE_SIZE);
 
-        Color[] colorVal = Tiles.myShuffle();
-        Color[] nColor = Tiles.myShuffle();
-        Color[] mColor = Tiles.myShuffle();
-        int pick = 0;
+            Color[] colorVal = Tiles.myShuffle();
+            Color[] nColor = Tiles.myShuffle();
+            Color[] mColor = Tiles.myShuffle();
+            int choice = 0;
 
-        for (int i = 0; i < BOARD_WIDTH; i++) {
-            for (int j = 0; j < BOARD_HEIGHT; j++) {
+            for (int i = 0; i < BOARD_WIDTH; i++) {
+                for (int j = 0; j < BOARD_HEIGHT; j++) {
 
-                Tiles tiles = new Tiles(nColor[pick], colorVal[pick], mColor[pick]);
-                tiles.setTranslateX(i * 100);
-                tiles.setTranslateY(j * 100);
-                root.getChildren().addAll(tiles);
-                pick++;
-                if (pick >= 10) {
-                    pick = 0;
+                    Tiles tiles = new Tiles();
+                    tiles.setTranslateX(i * 100);
+                    tiles.setTranslateY(j * 100);
+
+                    choice++;
+                    if (choice >= 10) {
+                        choice = 0;
+                    }
+
+                    tiles.setOnMouseClicked(event -> {
+                    if (mouseClick == 0) {
+                      tiles1 = tiles;
+                      System.out.println("First");
+                    }
+                    else if(mouseClick == 1) {
+                        tiles2 = tiles;
+                        System.out.println("Second");
+
+                        if(tiles1 == tiles2); {
+                            System.out.println("Three");
+                            tiles1.firstSet.setVisible(false);
+                            tiles1.secondSet.setVisible(false);
+                            tiles1.thirdSet.setVisible(false);
+                            tiles2.firstSet.setVisible(false);
+                            tiles2.secondSet.setVisible(false);
+                            tiles2.thirdSet.setVisible(false);
+
+                        }
+                        mouseClick = 0;
+
+                    }
+                        mouseClick++;
+                    });
+                    root.add(tiles, BOARD_WIDTH, BOARD_HEIGHT);
                 }
-            }
-        }
-        return root;
-    }
 
+            }
+            /*System.out.println("NumTiles: " + numTiles);
+*/
+            return root;
+        }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setScene(new Scene(theGame()));
         primaryStage.setTitle("My Tiles");
         primaryStage.setHeight(750);
+        primaryStage.setResizable(false);
         primaryStage.show();
 
-
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/mytiles/Tiles.java
+++ src/mytiles/Tiles.java
@@ -1,80 +1,145 @@
 package mytiles;
 
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 import java.util.*;
+import java.util.List;
 
-public class Tiles extends StackPane implements Comparable {
+public class Tiles extends StackPane {
 
-    private final Rectangle firstSet;
-    private final Rectangle secondSet;
-    private final Rectangle thirdSet;
+    Rectangle firstSet;
+    Rectangle secondSet;
+    Rectangle thirdSet;
+    private Color tileColor;
 
-    /*This Class just shuffles my colors in
-    * a random order*/
-    public static Color[] myShuffle() {
+     public static Color[] myShuffle() {
+        Color[] colorVal = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.DARKCYAN, Color.BLUEVIOLET, Color.PINK, Color.BURLYWOOD,
+                Color.ORANGE, Color.GREY};
 
-        Color[] colorVal = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.AZURE, Color.BLUEVIOLET, Color.PINK, Color.BURLYWOOD,
-                Color.ORANGE, Color.FUCHSIA};
         List<Color> list = Arrays.asList(colorVal);
         Collections.shuffle(list);
 
         return colorVal;
     }
 
-    /*This Tiles constructor is just creating rectangles
-    * to display on the board*/
-    public Tiles(Color color, Color nColor, Color mColor) {
+    public Color getTileColor() {
+         return tileColor;
+    }
 
+
+    /*This Class just shuffles my colors in
+     * a random order*/
+
+
+    /*This Tiles constructor is just creating rectangles
+     * to display on the board*/
+
+    public Tiles() {
 
         firstSet = new Rectangle(100, 100);
-        firstSet.setFill(color);
+        firstSet.setStroke(Color.BLACK);
+        firstSet.setFill(Color.ORANGE);
         firstSet.setArcHeight(20);
         firstSet.setArcWidth(20);
         firstSet.setStrokeWidth(3);
 
-
         secondSet = new Rectangle(70, 70);
         secondSet.setStroke(Color.BLACK);
-        secondSet.setFill(nColor);
+        secondSet.setFill(Color.BLUE);
         secondSet.setArcHeight(20);
         secondSet.setArcWidth(20);
-
+        secondSet.setStrokeWidth(3);
 
         thirdSet = new Rectangle(40, 40);
         thirdSet.setStroke(Color.BLACK);
-        thirdSet.setFill(mColor);
+        thirdSet.setFill(Color.DARKGOLDENROD);
         thirdSet.setArcHeight(20);
         thirdSet.setArcWidth(20);
-
-        List<Rectangle> rects = new ArrayList<>();
-        rects.add(firstSet);
-        rects.add(secondSet);
-        rects.add(thirdSet);
-
-        setOnMouseClicked(e -> firstSet.setVisible(true));
-        setOnMouseClicked(e -> firstSet.setStroke(Color.BLACK));
-
-        setOnMouseClicked(event -> {
-            if (firstSet.getFill().equals(secondSet.getFill()) ||
-                    firstSet.getFill().equals(thirdSet.getFill()) ||
-                    secondSet.getFill().equals(thirdSet.getFill())) {
-                setVisible(false);
-            } else {
-                setVisible(true);
-
-            }
-        });
-
+        thirdSet.setStrokeWidth(3);
 
         getChildren().addAll(firstSet, secondSet, thirdSet);
     }
 
 
-    @Override
-    public int compareTo(Object o) {
-        return 0;
+    public static void Compare(Tiles tiles) {
+
+
+        tiles.setOnMouseClicked(event -> tiles.firstSet.setStroke(Color.ORANGE));
+
+        if (tiles.firstSet.getFill().equals(tiles.secondSet.getFill())) {
+            tiles.firstSet.setVisible(false);
+            tiles.secondSet.setVisible(false);
+        }
+        if (tiles.secondSet.getFill().equals(tiles.thirdSet.getFill())) {
+            tiles.secondSet.setVisible(false);
+            tiles.thirdSet.setVisible(false);
+        }
+
+        if (tiles.firstSet.getFill().equals(tiles.thirdSet.getFill())) {
+            tiles.firstSet.setVisible(false);
+            tiles.thirdSet.setVisible(false);
+        }
     }
+
+
+
+       /* Tiles Tiles = null;
+        Tiles STiles = null;
+        HashMap<Integer, Rectangle> map1 = new HashMap<>();
+
+        map1.put(1, firstSet);
+        map1.put(2, secondSet);
+        map1.put(3, thirdSet);
+
+        HashMap<Integer, Rectangle> map2 = new HashMap<>();
+        map2.put(1, firstSet);
+        map2.put(2, secondSet);
+        map2.put(3, thirdSet);
+
+        if(map1.get(1).equals(map2.get(1))){
+            firstSet.setVisible(false);
+            secondSet.setVisible(false);
+            map1.remove(1, firstSet);
+            map2.remove(1, firstSet);
+
+        }
+        else {
+            map1.get(1).setVisible(true);
+            map2.get(2).setVisible(true);
+        }*/
+
+
+
+      /* HashSet<Rectangle> check = new HashSet<>(map2.values());
+       check.addAll(map1.values());
+       check.remove(map.)
+
+       HashSet<Rectangle> tile2 = new HashSet<>();
+       tile2.add(firstSet);
+       tile2.add(secondSet);
+       tile2.add(thirdSet);*/
+
+
+
+      /*  Tiles myTile = null;
+        Tiles otherTile;
+
+        List<Tiles> tiles = new ArrayList<>();
+        tiles.add(myTile);
+        tiles.add(otherTile);
+
+        if(myTile.firstSet.getFill().equals(otherTile.firstSet.getFill())) {
+            myTile.firstSet.setVisible();*/
+
+
+
 }
+
+
+
+
+
